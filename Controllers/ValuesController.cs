639c34d07d051f02de6a209f966345f459624b5a
TheSatishPawar <er.satishpawar@gmail.com>
2019-07-25 12:13:08 +0530

    removed values controller

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -11,7 +11,7 @@ namespace EventAPI.Controllers
     public class ValuesController : ControllerBase
     {
         // GET api/values
-        [HttpGet]
+        //[HttpGet]
         //public ActionResult<IEnumerable<string>> Get()
         //{
         //    return new string[] { "value1", "value2" };
@@ -25,21 +25,21 @@ namespace EventAPI.Controllers
         //}
 
         // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+        //// PUT api/values/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
 
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+        //// DELETE api/values/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
     }
 }
